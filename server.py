c42b1964ada6a62318e8cbaa233a401526273af2
Ege Meriç Erdoğan <egemeric@localhost.localdomain>
2020-01-31 10:52:37 +0300

    changed veriables name

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import socket
 import os
+PORT_=2020
+Listen_="0.0.0.0"
+Secret="bye"
 
 
 while True:
  s = socket.socket()
  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
  port = 12345
- s.bind(('0.0.0.0', port))
+ s.bind((Listen_, PORT_))
  s.listen(5)
  c, addr = s.accept()
  print("success:", addr)
@@ -15,7 +18,7 @@ while True:
     rcvdData = c.recv(5).decode()
     chack=rcvdData
 
-    if(chack[0:3]== "BYE" or chack[0:3]== "bye"): #secret code :D
+    if(chack[0:3]== Secret or chack[0:3]== Secret.lower()): #secret code :D
         os.system('dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock') #lock gnome 
         response='ok'
         c.send(response.encode())
